fbddcc8aa9d15141f72eb4b0cd0e498f74634b21
Solomon Ucko <solly.ucko@gmail.com>
2020-05-18 17:38:33 -0400

    0.3.4: Make Debug mostly match Python's repr

--- src/lib.rs
+++ src/lib.rs
@@ -126,7 +126,7 @@ pub struct Code {
 }
 
 #[rustfmt::skip]
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub enum Obj {
     None,
     StopIteration,
@@ -213,7 +213,136 @@ impl Obj {
     define_is! { is_frozenset     (FrozenSet(_))  }
     define_is! { is_code          (Code(_))       }
 }
-
+/// Should mostly match Python's repr
+///
+/// # Float, Complex
+/// - Uses `float('...')` instead of `...` for nan, inf, and -inf.
+/// - Uses Rust's float-to-decimal conversion.
+///
+/// # Bytes, String
+/// - Always uses double-quotes
+/// - Escapes both kinds of quotes
+///
+/// # Code
+/// - Uses named arguments for readability
+/// - lnotab is formatted as bytes(...) with a list of integers, instead of a bytes literal
+impl fmt::Debug for Obj {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::None => write!(f, "None"),
+            Self::StopIteration => write!(f, "StopIteration"),
+            Self::Ellipsis => write!(f, "Ellipsis"),
+            Self::Bool(true) => write!(f, "True"),
+            Self::Bool(false) => write!(f, "False"),
+            Self::Long(x) => write!(f, "{}", x),
+            &Self::Float(x) => python_float_repr_full(f, x),
+            &Self::Complex(x) => python_complex_repr(f, x),
+            Self::Bytes(x) => python_bytes_repr(f, x),
+            Self::String(x) => python_string_repr(f, x),
+            Self::Tuple(x) => python_tuple_repr(f, x),
+            Self::List(x) => f.debug_list().entries(x.read().unwrap().iter()).finish(),
+            Self::Dict(x) => f.debug_map().entries(x.read().unwrap().iter()).finish(),
+            Self::Set(x) => f.debug_set().entries(x.read().unwrap().iter()).finish(),
+            Self::FrozenSet(x) => python_frozenset_repr(f, x),
+            Self::Code(x) => python_code_repr(f, x),
+        }
+    }
+}
+fn python_float_repr_full(f: &mut fmt::Formatter, x: f64) -> fmt::Result {
+    python_float_repr_core(f, x)?;
+    if x.fract() == 0. {
+        write!(f, ".0")?;
+    };
+    Ok(())
+}
+fn python_float_repr_core(f: &mut fmt::Formatter, x: f64) -> fmt::Result {
+    if x.is_nan() {
+        write!(f, "float('nan')")
+    } else if x.is_infinite() {
+        if x.is_sign_positive() {
+            write!(f, "float('inf')")
+        } else {
+            write!(f, "-float('inf')")
+        }
+    } else {
+        // properly handle -0.0
+        if x.is_sign_negative() {
+            write!(f, "-")?;
+        }
+        write!(f, "{}", x.abs())
+    }
+}
+fn python_complex_repr(f: &mut fmt::Formatter, x: Complex<f64>) -> fmt::Result {
+    if x.re == 0. && x.re.is_sign_positive() {
+        python_float_repr_core(f, x.im)?;
+        write!(f, "j")?;
+    } else {
+        write!(f, "(")?;
+        python_float_repr_core(f, x.re)?;
+        if x.im >= 0. || x.im.is_nan() {
+            write!(f, "+")?;
+        }
+        python_float_repr_core(f, x.im)?;
+        write!(f, "j)")?;
+    };
+    Ok(())
+}
+fn python_bytes_repr(f: &mut fmt::Formatter, x: &[u8]) -> fmt::Result {
+    write!(f, "b\"")?;
+    for &byte in x.iter() {
+        match byte {
+            b'\t' => write!(f, "\\t")?,
+            b'\n' => write!(f, "\\n")?,
+            b'\r' => write!(f, "\\r")?,
+            b'\'' | b'"' | b'\\' => write!(f, "\\{}", char::from(byte))?,
+            b' '..=b'~' => write!(f, "{}", char::from(byte))?,
+            _ => write!(f, "\\x{:02x}", byte)?,
+        }
+    }
+    write!(f, "\"")?;
+    Ok(())
+}
+fn python_string_repr(f: &mut fmt::Formatter, x: &str) -> fmt::Result {
+    let original = format!("{:?}", x);
+    let mut last_end = 0;
+    // Note: the behavior is arbitrary if there are improper escapes.
+    for (start, _) in original.match_indices("\\u{") {
+        f.write_str(&original[last_end..start])?;
+        let len = original[start..].find('}').ok_or(fmt::Error)? + 1;
+        let end = start + len;
+        match len - 4 {
+            0..=2 => write!(f, "\\x{:0>2}", &original[start + 3..end - 1])?,
+            3..=4 => write!(f, "\\u{:0>4}", &original[start + 3..end - 1])?,
+            5..=8 => write!(f, "\\U{:0>8}", &original[start + 3..end - 1])?,
+            _ => panic!("Internal error: length of unicode escape = {} > 8", len),
+        }
+        last_end = end;
+    }
+    f.write_str(&original[last_end..])?;
+    Ok(())
+}
+fn python_tuple_repr(f: &mut fmt::Formatter, x: &[Obj]) -> fmt::Result {
+    if x.is_empty() {
+        f.write_str("()") // Otherwise this would get formatted into an empty string
+    } else {
+        let mut debug_tuple = f.debug_tuple("");
+        for o in x.iter() {
+            debug_tuple.field(&o);
+        }
+        debug_tuple.finish()
+    }
+}
+fn python_frozenset_repr(f: &mut fmt::Formatter, x: &HashSet<ObjHashable>) -> fmt::Result {
+    f.write_str("frozenset(")?;
+    if !x.is_empty() {
+        f.debug_set().entries(x.iter()).finish()?;
+    }
+    f.write_str(")")?;
+    Ok(())
+}
+fn python_code_repr(f: &mut fmt::Formatter, x: &Code) -> fmt::Result {
+    write!(f, "code(argcount={:?}, posonlyargcount={:?}, kwonlyargcount={:?}, nlocals={:?}, stacksize={:?}, flags={:?}, code={:?}, consts={:?}, names={:?}, varnames={:?}, freevars={:?}, cellvars={:?}, filename={:?}, name={:?}, firstlineno={:?}, lnotab=bytes({:?}))", x.argcount, x.posonlyargcount, x.kwonlyargcount, x.nlocals, x.stacksize, x.flags, Obj::Bytes(Arc::clone(&x.code)), x.consts, x.names, x.varnames, x.freevars, x.cellvars, x.filename, x.name, x.firstlineno, &x.lnotab)
+}
 /// This is a f64 wrapper suitable for use as a key in a (Hash)Map, since NaNs compare equal to
 /// each other, so it can implement Eq and Hash. `HashF64(-0.0) == HashF64(0.0)`.
 #[derive(Clone, Debug)]
@@ -276,7 +405,7 @@ where
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub enum ObjHashable {
     None,
     StopIteration,
@@ -290,7 +419,6 @@ pub enum ObjHashable {
     FrozenSet(Arc<HashableHashSet<ObjHashable>>),
     // etc.
 }
-
 impl TryFrom<&Obj> for ObjHashable {
     type Error = Obj;
 
@@ -317,6 +445,215 @@ impl TryFrom<&Obj> for ObjHashable {
             ))),
             x => Err(x.clone()),
         }
+    }
+}
+impl fmt::Debug for ObjHashable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::None => write!(f, "None"),
+            Self::StopIteration => write!(f, "StopIteration"),
+            Self::Ellipsis => write!(f, "Ellipsis"),
+            Self::Bool(true) => write!(f, "True"),
+            Self::Bool(false) => write!(f, "False"),
+            Self::Long(x) => write!(f, "{}", x),
+            Self::Float(x) => python_float_repr_full(f, x.0),
+            Self::Complex(x) => python_complex_repr(
+                f,
+                Complex {
+                    re: x.re.0,
+                    im: x.im.0,
+                },
+            ),
+            Self::String(x) => python_string_repr(f, x),
+            Self::Tuple(x) => python_tuple_hashable_repr(f, x),
+            Self::FrozenSet(x) => python_frozenset_repr(f, &x.0),
+        }
+    }
+}
+fn python_tuple_hashable_repr(f: &mut fmt::Formatter, x: &[ObjHashable]) -> fmt::Result {
+    if x.is_empty() {
+        f.write_str("()") // Otherwise this would get formatted into an empty string
+    } else {
+        let mut debug_tuple = f.debug_tuple("");
+        for o in x.iter() {
+            debug_tuple.field(&o);
+        }
+        debug_tuple.finish()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Code, CodeFlags, Obj, ObjHashable};
+    use num_bigint::BigInt;
+    use num_complex::Complex;
+    use std::{
+        collections::{HashMap, HashSet},
+        sync::{Arc, RwLock},
+    };
+
+    #[test]
+    fn test_debug_repr() {
+        assert_eq!(format!("{:?}", Obj::None), "None");
+        assert_eq!(format!("{:?}", Obj::StopIteration), "StopIteration");
+        assert_eq!(format!("{:?}", Obj::Ellipsis), "Ellipsis");
+        assert_eq!(format!("{:?}", Obj::Bool(true)), "True");
+        assert_eq!(format!("{:?}", Obj::Bool(false)), "False");
+        assert_eq!(
+            format!("{:?}", Obj::Long(Arc::new(BigInt::from(-123)))),
+            "-123"
+        );
+        assert_eq!(format!("{:?}", Obj::Tuple(Arc::new(vec![]))), "()");
+        assert_eq!(
+            format!("{:?}", Obj::Tuple(Arc::new(vec![Obj::Bool(true)]))),
+            "(True,)"
+        );
+        assert_eq!(
+            format!(
+                "{:?}",
+                Obj::Tuple(Arc::new(vec![Obj::Bool(true), Obj::None]))
+            ),
+            "(True, None)"
+        );
+        assert_eq!(
+            format!(
+                "{:?}",
+                Obj::List(Arc::new(RwLock::new(vec![Obj::Bool(true)])))
+            ),
+            "[True]"
+        );
+        assert_eq!(
+            format!(
+                "{:?}",
+                Obj::Dict(Arc::new(RwLock::new(
+                    vec![(
+                        ObjHashable::Bool(true),
+                        Obj::Bytes(Arc::new(Vec::from(b"a" as &[u8])))
+                    )]
+                    .into_iter()
+                    .collect::<HashMap<_, _>>()
+                )))
+            ),
+            "{True: b\"a\"}"
+        );
+        assert_eq!(
+            format!(
+                "{:?}",
+                Obj::Set(Arc::new(RwLock::new(
+                    vec![ObjHashable::Bool(true)]
+                        .into_iter()
+                        .collect::<HashSet<_>>()
+                )))
+            ),
+            "{True}"
+        );
+        assert_eq!(
+            format!(
+                "{:?}",
+                Obj::FrozenSet(Arc::new(
+                    vec![ObjHashable::Bool(true)]
+                        .into_iter()
+                        .collect::<HashSet<_>>()
+                ))
+            ),
+            "frozenset({True})"
+        );
+        assert_eq!(format!("{:?}", Obj::Code(Arc::new(Code {
+            argcount: 0,
+            posonlyargcount: 1,
+            kwonlyargcount: 2,
+            nlocals: 3,
+            stacksize: 4,
+            flags: CodeFlags::NESTED | CodeFlags::COROUTINE,
+            code: Arc::new(Vec::from(b"abc" as &[u8])),
+            consts: Arc::new(vec![Obj::Bool(true)]),
+            names: vec![],
+            varnames: vec![Arc::new("a".to_owned())],
+            freevars: vec![Arc::new("b".to_owned()), Arc::new("c".to_owned())],
+            cellvars: vec![Arc::new("de".to_owned())],
+            filename: Arc::new("xyz.py".to_owned()),
+            name: Arc::new("fgh".to_owned()),
+            firstlineno: 5,
+            lnotab: Arc::new(vec![255, 0, 45, 127, 0, 73]),
+        }))), "code(argcount=0, posonlyargcount=1, kwonlyargcount=2, nlocals=3, stacksize=4, flags=NESTED | COROUTINE, code=b\"abc\", consts=[True], names=[], varnames=[\"a\"], freevars=[\"b\", \"c\"], cellvars=[\"de\"], filename=\"xyz.py\", name=\"fgh\", firstlineno=5, lnotab=bytes([255, 0, 45, 127, 0, 73]))");
+    }
+
+    #[test]
+    fn test_float_debug_repr() {
+        assert_eq!(format!("{:?}", Obj::Float(1.23)), "1.23");
+        assert_eq!(format!("{:?}", Obj::Float(f64::NAN)), "float('nan')");
+        assert_eq!(format!("{:?}", Obj::Float(f64::INFINITY)), "float('inf')");
+        assert_eq!(format!("{:?}", Obj::Float(-f64::INFINITY)), "-float('inf')");
+        assert_eq!(format!("{:?}", Obj::Float(0.0)), "0.0");
+        assert_eq!(format!("{:?}", Obj::Float(-0.0)), "-0.0");
+    }
+
+    #[test]
+    fn test_complex_debug_repr() {
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 2., im: 1. })),
+            "(2+1j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 0., im: 1. })),
+            "1j"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 2., im: 0. })),
+            "(2+0j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 0., im: 0. })),
+            "0j"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -2., im: 1. })),
+            "(-2+1j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -2., im: 0. })),
+            "(-2+0j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 2., im: -1. })),
+            "(2-1j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 0., im: -1. })),
+            "-1j"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -2., im: -1. })),
+            "(-2-1j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: 0., im: -1. })),
+            "-1j"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -2., im: 0. })),
+            "(-2+0j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -0., im: 1. })),
+            "(-0+1j)"
+        );
+        assert_eq!(
+            format!("{:?}", Obj::Complex(Complex { re: -0., im: -1. })),
+            "(-0-1j)"
+        );
+    }
+
+    #[test]
+    fn test_bytes_string_debug_repr() {
+        assert_eq!(format!("{:?}", Obj::Bytes(Arc::new(Vec::from(
+                            b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe" as &[u8]
+                            )))),
+        "b\"\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x0b\\x0c\\r\\x0e\\x0f\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f !\\\"#$%&\\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\x7f\\x80\\x81\\x82\\x83\\x84\\x85\\x86\\x87\\x88\\x89\\x8a\\x8b\\x8c\\x8d\\x8e\\x8f\\x90\\x91\\x92\\x93\\x94\\x95\\x96\\x97\\x98\\x99\\x9a\\x9b\\x9c\\x9d\\x9e\\x9f\\xa0\\xa1\\xa2\\xa3\\xa4\\xa5\\xa6\\xa7\\xa8\\xa9\\xaa\\xab\\xac\\xad\\xae\\xaf\\xb0\\xb1\\xb2\\xb3\\xb4\\xb5\\xb6\\xb7\\xb8\\xb9\\xba\\xbb\\xbc\\xbd\\xbe\\xbf\\xc0\\xc1\\xc2\\xc3\\xc4\\xc5\\xc6\\xc7\\xc8\\xc9\\xca\\xcb\\xcc\\xcd\\xce\\xcf\\xd0\\xd1\\xd2\\xd3\\xd4\\xd5\\xd6\\xd7\\xd8\\xd9\\xda\\xdb\\xdc\\xdd\\xde\\xdf\\xe0\\xe1\\xe2\\xe3\\xe4\\xe5\\xe6\\xe7\\xe8\\xe9\\xea\\xeb\\xec\\xed\\xee\\xef\\xf0\\xf1\\xf2\\xf3\\xf4\\xf5\\xf6\\xf7\\xf8\\xf9\\xfa\\xfb\\xfc\\xfd\\xfe\""
+        );
+        assert_eq!(format!("{:?}", Obj::String(Arc::new(String::from(
+                            "\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f")))),
+                            "\"\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t\\n\\x0b\\x0c\\r\\x0e\\x0f\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1a\\x1b\\x1c\\x1d\\x1e\\x1f !\\\"#$%&\\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\x7f\"");
     }
 }
 
